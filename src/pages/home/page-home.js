01e27cd73c3bc15eb70aa0c8cbc3fb4438d76e79
JhonnDevJs <153306997+JhonEG-17@users.noreply.github.com>
2026-03-04 16:14:03 -0600

    Remove unused app-modal-travelpass component imports across multiple pages to streamline codebase and improve performance.

--- src/pages/home/page-home.js
+++ src/pages/home/page-home.js
@@ -2,7 +2,6 @@
 
 import "../../components/app-header.js?v=1.1.3";
 import "../../components/app-cookies-policy.js?v=1.0.3";
-import "../../components/app-modal-travelpass.js?v=1.0.2";
 import "../../components/app-button-whats.js?v=1.0.2";
 import "../../components/app-button-eva-trip.js?v=1.0.2";
 import "../../components/app-footer.js?v=1.0.2";
@@ -19,7 +18,6 @@ import "../../components/app-other-services.js";
 import "../../components/app-blog.js";
 import "../../components/app-img-shop-left.js";
 import "../../components/app-section-title.js";
-
 
 class PageHome extends HTMLElement {
 	connectedCallback() {
